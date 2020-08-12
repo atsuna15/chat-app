7fb84f6d87474545beec4b81fe367f56fc43afbf
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-08-12 12:28:43 +0900

    ああ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def edit
   end
 
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -16,6 +17,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-
 
 end
