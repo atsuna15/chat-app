6d55905efcc6107e1dab3006c00ad62020700d1d
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-08-16 16:58:41 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "messages#index"
+  root "rooms#index"
   resources :users, only: [:edit, :update,]
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create,]
 
 end
