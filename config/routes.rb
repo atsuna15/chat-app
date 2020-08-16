37d84fecc6c6825262fa0d8eb6db91bc0444af02
atsuna15 <69102309+atsuna15@users.noreply.github.com>
2020-08-16 18:50:45 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  resources :rooms, only: [:new, :create,]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 
 end
